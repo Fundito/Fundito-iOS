2034555719688eafc31c996c95507c37627ac6ca
MJ Studio <mym0404@gmail.com>
2019-12-22 21:01:45 +0900

    init Pod

---
+++ Fundito/EXKit/UIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController.swift
+//  Fundito
+//
+//  Created by 문명주 on 22/12/2019.
+//  Copyright © 2019 Fundito. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController{
+
+}
