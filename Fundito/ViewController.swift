84e26453085a03da78de66b6739c66a8715478c6
MJ Studio <mym0404@gmail.com>
2019-12-22 16:58:03 +0900

    initial commit

---
+++ Fundito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fundito
+//
+//  Created by 문명주 on 22/12/2019.
+//  Copyright © 2019 Fundito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
