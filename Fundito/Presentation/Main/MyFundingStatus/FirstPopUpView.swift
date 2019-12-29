fd771b64a2981fb13d1bf8e703f3de4672ac19aa
hyex <hyexkim@gmail.com>
2019-12-29 15:50:16 +0900

    add view with pan

---
+++ Fundito/Presentation/Main/MyFundingStatus/FirstPopUpView.swift
@@ -0,0 +1,21 @@
+//
+//  FirstPopUpView.swift
+//  Fundito
+//
+//  Created by kimhyeji on 12/29/19.
+//  Copyright © 2019 Fundito. All rights reserved.
+//
+
+import UIKit
+
+class FirstPopUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
